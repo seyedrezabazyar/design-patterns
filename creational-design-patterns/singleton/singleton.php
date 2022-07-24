6a9c1aa123a37a7b69f7f9a542f840e4c82963f3
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-07-24 13:28:04 +0430

    Add singleton example

---
+++ creational-design-patterns/singleton/singleton.php
@@ -0,0 +1,32 @@
+<?php
+
+class Singleton
+{
+    private static $instance;
+    public static function getInstance()
+    {
+        if (self::$instance == null){
+            self::$instance = new static;
+        }
+        return self::$instance;
+    }
+}
+
+class Config extends Singleton
+{
+    public function getData()
+    {
+        return [
+            'host' => '127.0.0.1'
+        ];
+    }
+}
+
+$config = Config::getInstance();
+$config1 = Config::getInstance();
+$config2 = new Config();
+
+var_dump($config);
+var_dump($config->getData()); // array(1) { 'host' => string(9) "127.0.0.1" }
+var_dump($config === $config1); // bool(true)
+var_dump($config === $config2); // bool(false)
